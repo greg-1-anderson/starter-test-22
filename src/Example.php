f4d4970068a45e2677bb0775e136875a4aba14d6
Greg Anderson <greg.1.anderson@greenknowe.org>
2018-03-20 20:33:33 -0700

    Initial commit.

--- src/Example.php
+++ src/Example.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace ExampleProject;
+namespace StarterTest22;
 
 class Example
 {
--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace ExampleProject;
+namespace StarterTest22;
 
 use PHPUnit\Framework\TestCase;
 
